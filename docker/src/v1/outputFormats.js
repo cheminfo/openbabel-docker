8ab5c8da8e243c3773e1d78517ad2dc0477b7e13
Luc Patiny <luc@patiny.com>
2022-08-09 09:37:55 +0200

    fix: route with outputFormats

--- docker/src/v1/outputFormats.js
+++ docker/src/v1/outputFormats.js
@@ -1,7 +1,7 @@
 import getOutputFormats from './getOutputFormats.js';
 
 export default function outputFormats(fastify) {
-  fastify.get('/v1/inputFormat', {}, (request, response) => {
+  fastify.get('/v1/outputFormats', {}, (request, response) => {
     response.send(getOutputFormats());
   });
 }
